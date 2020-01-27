30cd0c4103845517bb398eb00eddd8ebccba2700
Lewis Nangle <lewisnangle1@gmail.com>
2020-01-27 15:33:42 +0000

    First Commit

---
+++ src/Square.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './square.css';
+
+
+class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value:null,
+        }
+    }
+
+    render(){
+        return(
+
+            <button className="square" 
+            onClick = {()=>{this.setState({value:'X'})}}
+            >
+                {this.state.value}
+            </button>
+        );
+
+    }
+
+
+
+}
+
+
+export default Square;

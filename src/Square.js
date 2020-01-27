6a91677a5f27c0829b7426e00ab5aa9c4753da57
Lewis Nangle <lewisnangle1@gmail.com>
2020-01-27 15:49:54 +0000

    Handling clicks on squares

--- src/Square.js
+++ src/Square.js
@@ -3,20 +3,15 @@ import './square.css';
 
 
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value:null,
-        }
-    }
 
     render(){
         return(
 
-            <button className="square" 
-            onClick = {()=>{this.setState({value:'X'})}}
+            <button 
+                className="square" 
+                onClick = {()=>this.props.onClick()}
             >
-                {this.state.value}
+            {this.props.value}
             </button>
         );
 
